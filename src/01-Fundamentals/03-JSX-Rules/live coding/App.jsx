7acf5ff05a8405dc6955c7d8507a20627953be95
zhangzhengmao <zhangzhengmao.can@gmail.com>
2025-08-26 16:56:53 -0700

    Adding 03-JSXRules

---
+++ src/01-Fundamentals/03-JSX-Rules/live coding/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import JSXRules from "../exercise/solution/JSXRules";
+
+const App = () => {
+  return (
+    <>
+      <JSXRules />
+    </>
+  );
+};
+
+export default App;
